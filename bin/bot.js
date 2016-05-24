e8b5b689661969dbd5207e4612c2e10299c0d938
Darshan Soni <darshan@alumni.ubc.ca>
2016-05-23 21:02:20 -0700

    first commit

---
+++ bin/bot.js
@@ -0,0 +1,17 @@
+# bin/bot.js
+
+'use strict';
+
+var RamsayBot = require('../lib/ramsaybot');
+
+var token = process.env.BOT_API_KEY;
+var dbPath = process.env.BOT_DB_PATH;
+var name = process.env.BOT_NAME;
+
+var norrisbot = new RamsayBot({
+    token: token,
+    dbPath: dbPath,
+    name: name
+});
+
+norrisbot.run();
